dca6c5b03ccd206955038538ec8caaf6ebe6e4a9
Apollo013 <paul.j.millar@gmail.com>
2015-12-04 13:22:04 +0000

    Homework 01
    Homework for notification exercise

---
+++ RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        #region Field Members
+        List<Order> orderItems;
+        #endregion
+
+        #region Properties
+
+        public List<Order> OrderItems
+        {
+            get { return this.orderItems; }
+            set
+            {
+                if(value != orderItems)
+                {
+                    this.orderItems = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Method Members
+
+        protected override void OnDataLoaded()
+        {
+            this.OrderItems = base.Repository.Orders;
+        }
+
+        #endregion
+    }
+}
---
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {
+        #region Field Members
+
+        private List<MenuItem> menuItems;
+        private List<MenuItem> currentlySelectedMenuItems;
+
+        #endregion
+
+        #region Properties
+
+        public List<MenuItem> MenuItems
+        {
+            get { return this.menuItems; }
+
+            set
+            {
+                if(value != menuItems)
+                {
+                    this.menuItems = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        public List<MenuItem> CurrentlySelectedMenuItems
+        {
+            get { return this.currentlySelectedMenuItems; }
+
+            set
+            {
+                if (value != currentlySelectedMenuItems)
+                {
+                    this.currentlySelectedMenuItems = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Method Members
+
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+            this.CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+        }
+
+        #endregion
+    }
+}
